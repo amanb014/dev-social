e5cab4c7054518ce13e9e9901f1b0c921203fd0e
Aman Bhimani <amanb014@gmail.com>
2021-01-16 20:29:28 -0500

    firebase with express app deployment

--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -1,9 +1,12 @@
 import * as functions from "firebase-functions";
-
+import * as express from 'express';
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+const exp = express();
+
+exp.get('/api', (request, response) => {
+    response.status(200).send('Hello World');
 });
+
+export const app = functions.https.onRequest(exp);
